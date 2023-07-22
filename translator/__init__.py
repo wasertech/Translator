d21a0604339d4fa4fb24623cedf7eea304526e17
wasertech <danny@waser.tech>
2023-07-22 16:56:13 +0200

    v0.3.4b1

--- translator/__init__.py
+++ translator/__init__.py
@@ -1,6 +1,6 @@
 from translator.translate import Translator
 from translator.language import get_nllb_lang
 
-__version__ = "0.3.3b6"
+__version__ = "0.3.4b1"
 
 LANGS = get_nllb_lang()
--- translator/translate.py
+++ translator/translate.py
@@ -20,9 +20,9 @@ class Translator:
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
         self.logger.debug(f"{self.device}")
         self.logger.debug("Loading model...")
-        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_id, max_length=max_length, device=self.device)
         self.logger.debug("Loading tokenizer...")
-        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id, max_length=max_length)
         self.logger.debug("Setting up translation pipeline...")
         self.translator = pipeline(
             "translation",
